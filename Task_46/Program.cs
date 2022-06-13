075ee809231e76f37814b2957b0565d56ec07625
Margarita <margarita_anis@bk.ru>
2022-06-13 20:40:47 +0300

    Add project Task_46

---
+++ Task_46/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте двумерный массив размером m x n, заполненный случайными целыми числами.
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(10);
+        }
+    } 
+    return array;
+} 
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arr = GetArray(5, 7);
+PrintArray(arr);
