a078b5a955d47c4911eacff85be65162212a533e
Margarita <margarita_anis@bk.ru>
2022-06-19 22:49:00 +0500

    Add HOMEWORK Task_52

---
+++ Task_52/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом
+// столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого
+// столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine("Двумерный массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAvarage(int[,] array)
+{
+    double sum = 0;
+    double avarage = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            avarage = sum / array.GetLength(0);
+        }
+    }
+    Console.Write($"Среднее арифметическое: {avarage.ToString("F" + 1)}");
+}
+
+int[,] arr = GetArray(3, 3);
+PrintArray(arr);
+FindAvarage(arr);
